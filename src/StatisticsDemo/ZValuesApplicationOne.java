3a92a08005e94ff653991262560d0000bc8e4ce9
mokhc <mokhonchuen@yahoo.com>
2022-02-03 18:21:03 +0800

    Revert "Delete ZValuesApplicationOne.java"
    This reverts commit 336e8256822906425392a5e287ad4397697850c2.

---
+++ src/StatisticsDemo/ZValuesApplicationOne.java
@@ -0,0 +1,14 @@
+package StatisticsDemo;
+
+public class ZValuesApplicationOne {
+	public static void main(String[] args) {
+		ZValues zv1 = new ZValues(0.5060);
+		zv1.printInfo();
+		
+		ZValues zv2 = new ZValues(0.4880);
+		zv2.printInfo();
+		
+		ZValues zv3 = new ZValues(0.5002);
+		zv3.printInfo();
+	}
+}
